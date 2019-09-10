4400e8e586bed5ea3f198592eebb3f47eed517a8
Joshua Hamann <haman127@sentinel.morris.umn.edu>
2019-09-10 12:17:27 -0500

    adds cat and pipeline into an awk then into the file failed_login_data.txt

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 dir=$1
 cd $dir
-for file in *.txt
-do
-	sed -E 's/\* ([a-zA-Z]+) ([a-zA-Z]+) ([a-zA-Z]+) computer_name sshd\[\d{5}\]: Failed password for invalid user ([a-zA-Z]+) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/1. \1\n2. \2\n/' < $file > failed_login_data.txt
-done
+cat var/log/secure* | awk 'match($0,/([a-zA-Z]+) ([a-zA-Z0-9]+) ([a-zA-Z0-9]+):\d\d:\d\d ([a-zA-Z_]+) sshd\[\d{5}\]: Failed password for( invalid user)? ([a-zA-Z]+) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/, groups) {print groups[1,2,3,5,6]}' > failed_login_data.txt
+#sed -E 's/\* ([a-zA-Z]+) ([a-zA-Z]+) ([a-zA-Z]+) [a-zA-Z] sshd\[\d{5}\]: Failed password for invalid user ([a-zA-Z]+) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/1\2\3\4\5/' < $file > failed_login_data.txt
+
+
