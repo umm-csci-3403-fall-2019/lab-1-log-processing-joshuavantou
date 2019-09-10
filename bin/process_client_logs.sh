5e868dc3977bac407680e16632385f46eea36abd
Joshua Hamann <haman127@sentinel.morris.umn.edu>
2019-09-10 15:47:00 -0500

    adds create_username_dist.sh, also makes it executable

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 dir=$1
 cd "$dir" || exit
-cat var/log/secure* | awk 'match($0,/([a-zA-Z]+) ([a-zA-Z0-9]+) ([a-zA-Z0-9]+):\d\d:\d\d ([a-zA-Z_]+) sshd\[\d{5}\]: Failed password for( invalid user)? ([a-zA-Z]+) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/, groups) {print groups[1,2,3,5,6]}' > failed_login_data.txt
-#sed -E 's/\* ([a-zA-Z]+) ([a-zA-Z]+) ([a-zA-Z]+) [a-zA-Z] sshd\[\d{5}\]: Failed password for invalid user ([a-zA-Z]+) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/1\2\3\4\5/' < $file > failed_login_data.txt
+cat var/log/secure* | sed -E 's/([a-zA-Z]+) ([0-9]+) ([0-9]+):\d\d:\d\d [a-zA-Z_]+ sshd\[\d{5}\]: Failed password for (invalid user )?([a-zA-Z0-9_-]*) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/1\2\3\4\5/' > failed_login_data.txt
+ 
+	
+#awk 'match($0,/([a-zA-Z]+) ([0-9]+) ([0-9]+):\d\d:\d\d ([a-zA-Z_]+) sshd\[\d{5}\]: Failed password for( invalid user)? ([a-zA-Z]+) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/, groups) {print $1 $2 $3 $5 $6}' > failed_login_data.txt
+
 
 
