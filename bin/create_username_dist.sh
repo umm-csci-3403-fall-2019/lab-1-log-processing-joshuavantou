3d5cd414d3af7d14b44c4725ae7fb3060f692975
Joshua Hamann <haman127@athena.morris.umn.edu>
2019-09-11 18:26:03 -0500

    adds wrap_content.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 dir= $1
-awk 'match($0, /[a-zA-Z] [0-9 ]+ [0-9]+ ([a-zA-Z0-9]+) [0-9]+/, groups) {print groups[1]}' < r2_output.txt | sort | uniq -c | awk 'match($0, /     ([0-9]+) ([a-zA-Z0-9]+)/, groups) {print groups[2]" "groups[1]}' > username_dist.html
+cd $dir
+awk 'atch($0, /[a-zA-Z] [0-9 ]+ [0-9]+ ([a-zA-Z0-9]+) [0-9]+/, groups) {print groups[1]}' < r2_output.txt | sort | uniq -c | awk 'match($0, /     ([0-9]+) ([a-zA-Z0-9]+)/, groups) {print groups[2]" "groups[1]}' > username_dist.html
