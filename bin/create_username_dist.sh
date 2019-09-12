4733399860c822db54fbd05b84eb46d5701e9456
Joshua Hamann <haman127@athena.morris.umn.edu>
2019-09-11 21:58:36 -0500

    fixed shellcheck

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 dir=$1
 here=$(pwd)
-echo $here
-cd $dir
-tree
+cd "$dir" || exit
 cat */failed_login_data.txt | awk 'match($0, /[a-zA-Z] [0-9 ]+ [0-9]+ ([a-zA-Z0-9]+) [0-9]+/, groups) {print groups[1]}' | sort | uniq -c | awk 'match($0, /[ ]+([0-9]+) ([a-zA-Z0-9]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' > username_dist1.html
-cd $here
-./bin/wrap_contents.sh $dir/username_dist1.html html_components/username_dist $dir/username_dist.html
-rm $dir/username_dist1.html
+cd "$here" || exit
+./bin/wrap_contents.sh "$dir"/username_dist1.html html_components/username_dist "$dir"/username_dist.html
+rm "$dir"/username_dist1.html
