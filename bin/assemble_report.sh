d7eb9c91a63567911c1de463f67fc08c90d479bd
Joshua Hamann <haman127@avenger.morris.umn.edu>
2019-09-17 12:01:43 -0500

    Shell check Writes in the code necessary to work

---
+++ bin/assemble_report.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+DIR=$1
+HOME=$(pwd)
+cd "$DIR" || exit
+cat country_dist.html hours_dist.html username_dist.html > summary.html
+cd "$HOME" || exit
+./bin/wrap_contents.sh "$DIR"/summary.html html_components/summary_plots "$DIR"/failed_login_summary.html
+rm "$DIR"/summary.html
