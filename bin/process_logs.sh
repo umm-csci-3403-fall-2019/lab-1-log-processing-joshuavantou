c932cdfdf0eaf329dd8fcca2aca8f6fd602b8459
Joshua Hamann <haman127@discovery.morris.umn.edu>
2019-09-17 14:20:55 -0500

    adds code

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
-#SCRATCH=`mktemp --directory`
-mkdir test123
-cp $@ --t test123
-#SCRATCH2=`mktemp --directory`
-for f in test123/*_secure.tgz;
-do tar -xzf "$f";
-done	
+SCRATCH=$(mktemp --directory)
+HOME=$(pwd)
+mv $@ $SCRATCH
+~       
