49bc4406b1e5e8b5f0c75e1d1700641eedae596f
Joshua Hamann <haman127@avenger.morris.umn.edu>
2019-09-17 12:03:31 -0500

    Starter code for this file

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
-SCRATCH=`mktemp --directory`
-
+#SCRATCH=`mktemp --directory`
+mkdir test123
+cp $@ --t test123
+#SCRATCH2=`mktemp --directory`
+for f in test123/*_secure.tgz;
+do tar -xzf "$f";
+done	
