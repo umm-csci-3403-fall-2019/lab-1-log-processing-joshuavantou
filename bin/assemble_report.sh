28d6678400f5a41249301860f921eccd98acabf5
Joshua Hamann <haman127@tang.morris.umn.edu>
2019-09-18 15:42:42 -0500

    fixes regex to match user names

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -5,4 +5,4 @@ cd "$DIR" || exit
 cat country_dist.html hours_dist.html username_dist.html > summary.html
 cd "$HOME" || exit
 ./bin/wrap_contents.sh "$DIR"/summary.html html_components/summary_plots "$DIR"/failed_login_summary.html
-rm "$DIR"/summary.html
+rm "$DI"/summary.html
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -3,4 +3,4 @@ dir=$1
 cd "$dir" || exit
 #cat var/log/* | sed -E -n 's/([a-zA-Z]+) ([0-9]+) ([0-9]+):\d\d:\d\d [a-zA-Z_]+ sshd\[\d{5}\]: Failed password for (invalid user )?([a-zA-Z0-9_-]*) from ([a-zA-Z0-9.]+) port \d{4,5} ssh2/\1\2\3\5\6/p' > failed_login_data.txt
 
-cat var/log/* | awk 'match($0, /([a-zA-Z]+) ([0-9 ]+) ([0-9][0-9]):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for( invalid user)? ([a-zA-Z0-9]+) from ([a-zA-Z0-9.]+) port [0-9]+ ssh2/, groups) {print groups[1]" " groups[2]" "groups[3]" "groups[5]" "groups[6]}' > failed_login_data.txt
+cat var/log/* | awk 'match($0, /([a-zA-Z]+) ([0-9 ]+) ([0-9][0-9]):[0-9]+:[0-9]+ [a-zA-Z_]+ sshd\[[0-9]+\]: Failed password for( invalid user)? ([a-zA-Z0-9._-]+) from ([a-zA-Z0-9.]+) port [0-9]+ ssh2/, groups) {print groups[1]" " groups[2]" "groups[3]" "groups[5]" "groups[6]}' > failed_login_data.txt
