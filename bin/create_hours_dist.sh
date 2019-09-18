d6bb27e58840b718f5f8c29d1dbabeb990cd9680
xion1547 <xion1547@morris.umn.edu>
2019-09-18 14:09:32 -0500

    hanges regex and fixes the for loop in process logs

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -2,7 +2,7 @@
 dir=$1
 here=$(pwd)
 cd "$dir" || exit
-cat ./*/failed_login_data.txt | awk 'match($0, /[a-zA-Z] [0-9 ]+ ([0-9]+) [a-zA-Z0-9]+ [0-9]+/, groups) {print groups[1]}' | sort | uniq -c | awk 'match($0, /[ ]+([0-9]+) ([0-9]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' > hours_dist1.html
+cat ./*/failed_login_data.txt | awk 'match($0, /[a-zA-Z] [0-9 ]+ ([0-9_.-]+) [a-zA-Z0-9]+ [0-9]+/, groups) {print groups[1]}' | sort | uniq -c | awk 'match($0, /[ ]+([0-9_.-]+) ([0-9_.-]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, "groups[1]"]);"}' > hours_dist1.html
 cd "$here" || exit
 ./bin/wrap_contents.sh "$dir"/hours_dist1.html html_components/hours_dist "$dir"/hours_dist.html
 rm "$dir"/hours_dist1.html
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 HOME=$(pwd)
 SCRATCH=$(mktemp --directory)
-cp -r "$@" "$SCRATCH"
-cd "$SCRATCH"
-for f in ./* do
-	if [ -x "$f" ]; then
-		tar -xvf $f.tar.gz
-	fi
+#cp -r "$@" "$SCRATCH"
+#cd "$SCRATCH"
+for f in "$@" ;do
+	filename=$(basename "$f" "_secure.tgz")
+	mkdir "$SCRATCH"/"$filename"
+	tar -xzf $f -C "$SCRATCH"/"$filename"
+	./bin/process_client_logs.sh "$SCRATCH"/"$filename"
 done
-
+./bin/create_username_dist.sh "$SCRATCH"
+./bin/create_hours_dist.sh "$SCRATCH"
+./bin/create_country_dist.sh "$SCRATCH"
+./bin/assemble_report.sh "$SCRATCH"
+cp "$SCRATCH"/failed_login_summary.html failed_login_summary.html
+tree
+cat failed_login_summary.html
